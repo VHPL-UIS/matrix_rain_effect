e13b590a78e4e135c990e4c7ec66baf688a72144
VHPL-UIS <heidaripour.l.vahid@uis.no>
2025-07-03 09:50:50 +0200

    screen control

--- main.cpp
+++ main.cpp
@@ -2,19 +2,46 @@
 #include <thread>
 #include <chrono>
 
+void clearScreen()
+{
+    std::cout << "\033[2J\033[H";
+    std::cout.flush();
+}
+
+void hideCursor()
+{
+    std::cout << "\033[?25l";
+    std::cout.flush();
+}
+
+void showCursor()
+{
+    std::cout << "\033[?25h";
+    std::cout.flush();
+}
+
 int main()
 {
+    clearScreen();
+    hideCursor();
+
     // green color
     std::cout << "\033[32m";
 
-    std::cout << "Hello Matrix World!" << std::endl;
-    std::cout << "This text is green!" << std::endl;
+    std::cout << "Screen cleared and cursor hidden!" << std::endl;
+    std::cout << "Notice how clean this looks..." << std::endl;
 
     std::this_thread::sleep_for(std::chrono::seconds(3));
 
-    // reset color
+    clearScreen();
+    std::cout << "Screen cleared again!" << std::endl;
+
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+
+    // reset everything
     std::cout << "\033[0m";
-    std::cout << "Color reseted!" << std::endl;
+    showCursor();
+    clearScreen();
 
     return 0;
 }
